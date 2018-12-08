494e879d864dcf57fc2a09a7eae56a33f36a0258
VictorChiangTW <45275788+VictorChiangTW@users.noreply.github.com>
2018-12-08 09:09:35 +0800

    USA part

--- driving.py
+++ driving.py
@@ -5,3 +5,9 @@ if country == "台灣" or "臺灣" or "中華民國":
 		print("你可以開車")
 	else:
 		print("你還不能開車")
+elif country == "美國" or "USA":
+	if age >= 16:
+		print("你可以開車")
+	else:
+		print("你還不能開車")
+#不知道為什麼，美國的部分，輸入16，會進入到else的你還不能開車
